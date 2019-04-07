41520d2d7c7d6767633e0da04cd3af26ab5e8aac
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-04-07 14:18:19 +0200

    fix fraction size in reading value

--- multi.sv
+++ multi.sv
@@ -7,17 +7,18 @@ module multi(
 logic [22:0]Ofraction_HI; //fraction will neglict any bit after ofraction[22]
 logic [22:0]Ofraction_LO;
 logic Osign;
-logic [5:0]Opower;
+logic [7:0]Opower;
 logic [22:0]Afraction ;
 logic [22:0]Bfraction ;
-logic [5:0] Apower;
-logic [5:0] Bpower;
+logic [7:0] Apower;
+logic [7:0] Bpower;
 logic Asign;
 logic Bsign;
 
 logic carryExp;
 logic carryFra;
 
+// logic [32:0]big;
 initial begin
 {Asign,Apower,Afraction} = a;
 {Bsign,Bpower,Bfraction} = b;	
@@ -27,9 +28,13 @@ always @(a or b) begin
 	
 	{carryFra,Ofraction_HI,Ofraction_LO} = (Afraction*Bfraction + Afraction<<23 + Bfraction<<23);
 	
+	// Opower = Apower + Bpower;
+
+	{carryExp,Opower} = Apower + Bpower -127;
 	
-	{carryExp,Opower} = Apower + Bpower + carryFra;
-	
+	// $display(0111010);
+	// $display(big);
+
 	Osign = Asign ^ Bsign;
 	// normlizing number for case of ofraction > 1
 	if (carryFra)
