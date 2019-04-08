b01277bed12ef63c0e6eaf0064324cc129abb425
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-04-08 21:26:11 +0200

    working

--- multi.sv
+++ multi.sv
@@ -15,10 +15,17 @@ logic [7:0] Bpower;
 logic Asign;
 logic Bsign;
 
-logic carryExp;
-logic carryFra;
+logic [1:0]carryExp;
+logic [2:0]carryFra;
 
+// TODO delete 
 // logic [32:0]big;
+// logic [45:0]mu;
+// logic [45:0]sh1;
+// logic [45:0]sh2;
+// logic [50:0]su;
+
+
 initial begin
 {Asign,Apower,Afraction} = a;
 {Bsign,Bpower,Bfraction} = b;	
@@ -26,24 +33,26 @@ end
 
 always @(a or b) begin
 	
-	{carryFra,Ofraction_HI,Ofraction_LO} = (Afraction*Bfraction + Afraction<<23 + Bfraction<<23);
+	// adding brances solve an problem in order of adding
+	{carryFra,Ofraction_HI,Ofraction_LO} = ((Afraction*Bfraction) + (Afraction<<23) + (Bfraction<<23));
 	
-	// Opower = Apower + Bpower;
-
-	{carryExp,Opower} = Apower + Bpower -127;
+	// exponent is biased by 127 so (000000011)
+	//               represented in (100000010)
+	{carryExp,Opower} = Apower + Bpower - 127 + carryFra;
 	
-	// $display(0111010);
-	// $display(big);
-
 	Osign = Asign ^ Bsign;
+	
 	// normlizing number for case of ofraction > 1
 	if (carryFra)
-		op = {Osign , Opower , Ofraction_HI<<1};
-	else
-		op = {Osign , Opower , Ofraction_HI};
+		Ofraction_HI = Ofraction_HI>>1;
+	
 	//check the exponent
 	if (carryExp)
 		$display("overflow"); 
+		
+	op = {Osign , Opower , Ofraction_HI};
+	
+	
 end
 	
 endmodule
