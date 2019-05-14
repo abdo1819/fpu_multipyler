dcef83e5c6547930b976a4e76c7841bdc75a1870
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-05-14 15:05:44 +0200

    handling zero and return inf at overflow

--- multi.sv
+++ multi.sv
@@ -28,61 +28,69 @@ logic carryExp;
 logic [2:0] carryFra;
 
 logic [31:0] nan = 32'b01111111100000000000000000000001 ;
-
+logic [31:0] zero= 32'b00000000000000000000000000000000 ;
 always @(a or b) begin
-
+	
 	{Asign,Apower,Afraction} = a;
 	{Bsign,Bpower,Bfraction} = b;	
+	
+	Osign = Asign ^ Bsign;
 
 	//TODO: special cases
 	// nan or nan => nan
 	// inf and 0  => nan
-	// inf and anything => inf
-	// 0   and 0  => 0
+	// inf and other => inf
+	// 0   and other  => 0
 
-	// if or b is nan
-	if ((ones_Apower & ~zero_Afraction) | (ones_Bpower & ~zero_Bfraction)) 
-		// nan 255 exp , non zero frac 
-		op = nan;
-
-	else if ((ones_Apower & zero_Afraction)) // a is inf (255,zeros)
-	begin
-		if ((zero_Bpower & zero_Bfraction)) // b is 0 (0,0)
+	// if or b is nan (255,not zero)
+	if ((ones_Apower && ~zero_Afraction) || (ones_Bpower && ~zero_Bfraction)) 
+	
+	op = nan;
+	
+	else if ((ones_Apower && zero_Afraction)) // a is inf (255,zeros)
+		begin
+		if ((zero_Bpower && zero_Bfraction)) // b is 0 (0,0)
 			op = nan; // inf and 0 => nan
 		else
 			op = a;  // inf and otherthings => inf
-
-	else if ((ones_Bpower & zero_Bfraction)) // b is inf (255,zeros)
-	begin
-		if ((zero_Apower & zero_Afraction)) // a is 0
+		end
+	else if ((ones_Bpower && zero_Bfraction)) // b is inf (255,zeros)
+		begin
+		if ((zero_Apower && zero_Afraction)) // a is 0
 			op = nan; // inf and 0 => nan
 		else
 			op = b; // inf and otherthings => inf
-		
-	// clac fraction (1+f1)(1+f2)=(1+f1*f2+f1+f2) 
-	{carryFra,Ofraction_HI,Ofraction_LO} = ((Afraction*Bfraction) + (Afraction<<23) + (Bfraction<<23));
-	
-	// exponent is biased by 127 so (000000011)
-	//               represented in (100000010)
-	{carryExp,Opower} = Apower + Bpower - 8'd127 ;
-		
-	
-	Osign = Asign ^ Bsign;
-	
-	// normlizing number for case of ofraction > 2
-	if (carryFra)
-		begin
-		{carryExp,Opower} = Opower + 8'b1; 
-		Ofraction_HI = Ofraction_HI>>1;
-		Ofraction_HI[0] = Ofraction_LO[22];
 		end
-	//check the exponent
-	if (carryExp)
-		$display("overflow"); 
+	// if or b is zero (0,0)
+	else if ((zero_Apower && zero_Afraction)||(zero_Bpower && zero_Bfraction))
+		op = zero
+
+	else
+	begin
+		// clac fraction (1+f1)(1+f2)=(1+f1*f2+f1+f2) 
+		{carryFra,Ofraction_HI,Ofraction_LO} = ((Afraction*Bfraction) + (Afraction<<23) + (Bfraction<<23));
 		
-	op = {Osign , Opower , Ofraction_HI};
-	
-	
-end
-	
+		// exponent is biased by 127 so (000000011)
+		//               represented in (100000010)
+		{carryExp,Opower} = Apower + Bpower - 8'd127 ;
+			
+		
+		
+		// normlizing number for case of ofraction > 2
+		if (carryFra)
+			begin
+			{carryExp,Opower} = Opower + 8'b1; 
+			Ofraction_HI = Ofraction_HI>>1;
+			Ofraction_HI[0] = Ofraction_LO[22];
+			end
+		//check the exponent
+		//if overflow occurs, return inf
+		if (carryExp)
+			$display("overflow"); 
+			Opower = 8'b11111111
+			Ofraction_HI = 23'b0
+		op = {Osign , Opower , Ofraction_HI};
+
+		end
+	end
 endmodule
