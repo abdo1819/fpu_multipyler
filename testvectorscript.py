2570d5ec7802eb6fb6f5273860bbea031068c04d
Abdullah khaled <34966791+A-Siam@users.noreply.github.com>
2019-06-07 16:50:04 +0200

    Update testvectorscript.py

--- testvectorscript.py
+++ testvectorscript.py
@@ -11,19 +11,15 @@ else:
 		if len(y)!=2:
 			print("use 2 operands")
 		else:
-			z2 = ctypes.c_int32.from_buffer(ctypes.c_float(int(y[1]))).value
-			z1 = ctypes.c_int32.from_buffer(ctypes.c_float(int(y[0]))).value
-			z_result = ctypes.c_int32.from_buffer(ctypes.c_float(int(y[0]) * int(y[1]))).value
+			z2 = ctypes.c_int32.from_buffer(ctypes.c_float(float(y[1]))).value
+			z1 = ctypes.c_int32.from_buffer(ctypes.c_float(float(y[0]))).value
+			z_result = ctypes.c_int32.from_buffer(ctypes.c_float(float(y[0]) * float(y[1]))).value
 			print(hex(z1)+"_"+hex(z2)+"_"+hex(z_result))
-			fi = 0
 			if len(sys.argv) ==2:
 				if sys.argv[1] == "-t":
 					fi = open("testvector.tv","w+")
-					fi.write(hex(z1)+"_"+hex(z2)+"_"+hex(z_result))
+					fi.write(hex(z1)+"_"+hex(z2)+"_"+hex(z_result)+"\n")
 					exit()
 				else:
-					print("invalid argument")
-					exit()
-			else:
-				fi = open("testvector.tv","a+")					
-				fi.write(hex(z1)+"_"+hex(z2)+"_"+hex(z_result))
+					fi = open("testvector.tv","a+")					
+					fi.write(hex(z1)+"_"+hex(z2)+"_"+hex(z_result)+"\n")
