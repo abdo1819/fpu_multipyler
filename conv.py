3be2c7516a07c19fd353238f8fd7a03a4a6796cd
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-06-11 18:23:11 +0200

    use simpler special generator

--- conv.py
+++ conv.py
@@ -1,45 +1,62 @@
 import struct
 import sys
+import random
 
 class converter():
     ''' creating test vctor with special cases '''
     zero ='fds'
     def __init__(self):
-        self.zero = '00000000'
-        self.inf =  '7F800000'
-        self.inf_=  'FF800000'
-        self.undef ='7F800001'
+        '''initialize for special cases out as hex
+            # use case
+            > from conv import converter as c
+            > print(c.inf)
+             '7F800000'
+        '''
+        # self.zero = '00000000'
+        # self.inf =  '7F800000'
+        # self.inf_=  'FF800000'
+        # self.undef ='7F800001'
+
+        self.zero = float(0)
+        self.inf =  float('inf')
+        self.inf_=  float('-inf')
+        self.undef =float('NAN')
 
     def f_h(self,f):
-        '''convert floating numbers into 32'bit hex format '''
+        '''convert floating numbers into 32'bit hex format 
+        example :
+        > from conv import converter as c
+        > print(c.f_h(1.25))
+        '''
         return struct.pack('>f',f).hex() 
     
     def special(self):
-        special = [self.zero,self.inf,self.inf_,self.undef]
+        ''' generating list of all multiplication special cases
+            as string  in format  xxxxxxxx_xxxxxxxx_xxxxxxxx
+            # useing example for test vector
+            > f = open('mult.tv','w') 
+            > for row in c.special_mult():
+            >    f.write(row+'\\n')
+            > f.close()
+        '''
+  
+        sp = [self.zero,self.inf,self.inf_,self.undef]
         special_hex = []
-        for i in special:
-            for j in special:
-                if (i == self.undef or j == self.undef):#undef,any -> undef
-                    out = self.undef
-                elif((i == self.inf and j == self.zero) or (j == self.inf and i == self.zero) or
-                    (i == self.inf_ and j == self.zero) or (j == self.inf_ and i == self.zero)):
-                    out = self.undef
-                elif(i == self.inf and j== self.inf)or(i == self.inf_ and j== self.inf_):
-                    out = self.inf
-                elif (i == self.inf_ and j== self.inf)or (i == self.inf and j== self.inf_):
-                    out = self.inf_
-                elif (i == self.zero or j == self.zero):
-                    out = self.zero
-                else:
-                    out = 'error'
-
-                special_hex.append(i+'_'+j+'_'+out)
+        for i in sp:
+            for j in sp:
+                special_hex.append(self.f_h(i)+'_'+self.f_h(j)+'_'+self.f_h(i*j))
         return special_hex
 
+                
 
-
-    def print_(self):
-        print(self.zero+'_'+self.inf+'_'+self.inf_+'_'+self.undef)
+    
+    def randomtest_multi(self, n):
+        random_hex =[]
+        for i in range(n):
+            f1 = random.random()
+            f2 = random.random()
+            random_hex.append(self.f_h(f1)+'_'+self.f_h(f2)+'_'+self.f_h(f1*f2))
+        return random_hex
 
             
 if __name__ == "__main__":
@@ -50,7 +67,7 @@ if __name__ == "__main__":
         f2 = float(n[2])
         print(c.f_h(f1),'_',c.f_h(f2),'_',c.f_h(f1*f2),sep='')
     
-    f = open('mult.tv','w')
+    f = open('mult.tv','w') #write in new file
     for row in c.special():
         f.write(row+'\n')
     f.close()
