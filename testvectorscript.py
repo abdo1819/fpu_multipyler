71583045dc19a7a74c95a5a7ad3c774ad222ef52
Abdullah khaled <34966791+A-Siam@users.noreply.github.com>
2019-06-12 12:15:57 +0200

    fixing -ve numbers bug

--- testvectorscript.py
+++ testvectorscript.py
@@ -1,4 +1,4 @@
-import ctypes ,sys
+import struct ,sys
 if len(sys.argv) > 2:
 	print("invalid arguments!")
 else:
@@ -11,15 +11,15 @@ else:
 		if len(y)!=2:
 			print("use 2 operands")
 		else:
-			z2 = ctypes.c_int32.from_buffer(ctypes.c_float(float(y[1]))).value
-			z1 = ctypes.c_int32.from_buffer(ctypes.c_float(float(y[0]))).value
-			z_result = ctypes.c_int32.from_buffer(ctypes.c_float(float(y[0]) * float(y[1]))).value
-			print(hex(z1)+"_"+hex(z2)+"_"+hex(z_result))
+			z2 = struct.pack('>f',float(y[1])).hex() 
+			z1 = struct.pack('>f',float(y[0])).hex()
+			z_result =struct.pack('>f',float(float(y[0]) * float(y[1]))).hex() 
+			print(str(z1)+"_"+str(z2)+"_"+str(z_result))
 			if len(sys.argv) ==2:
 				if sys.argv[1] == "-t":
 					fi = open("testvector.tv","w+")
-					fi.write(hex(z1)+"_"+hex(z2)+"_"+hex(z_result)+"\n")
+					fi.write(str(z1)+"_"+str(z2)+"_"+str(z_result)+"\n")
 					exit()
 				else:
 					fi = open("testvector.tv","a+")					
-					fi.write(hex(z1)+"_"+hex(z2)+"_"+hex(z_result)+"\n")
+					fi.write(str(z1)+"_"+str(z2)+"_"+str(z_result)+"\n")
